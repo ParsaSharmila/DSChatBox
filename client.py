49885df9f6598fd2207667e958ef8d3bfeaee353
ParsaSharmila <44161437+ParsaSharmila@users.noreply.github.com>
2019-04-04 13:57:25 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,78 +1,78 @@
 #!/usr/bin/env python3
-"""client script"""
-import tkinter
-from threading import Thread
 from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+import tkinter
 
 
-def receive_message():
+def receive():
+    
     while True:
         try:
-            msg = socket.recv(1024).decode("utf8")
+            msg = client_socket.recv(1024).decode("utf8")
             msg_list.insert(tkinter.END, msg)
-        except OSError:
+        except OSError:  # Possibly client has left the chat.
             break
 
 
 def receive_file():
     while True:
         rfile = open("received_history.txt", "ab")
-        file_data = socket.recv(1024)
+        file_data = client_socket.recv(1024)
         rfile.write(file_data)
 
 
-def send_message(event=None):
-    msg = my_message.get()
-    my_message.set("")
-    socket.send(bytes(msg, "utf8"))
-    if msg == "{q}":
-        socket.close()
+def send(event=None): 
+    
+    msg = my_msg.get()
+    my_msg.set("")  # Clears input field.
+    client_socket.send(bytes(msg, "utf8"))
+    if msg == "{quit}":
+        client_socket.close()
         top.quit()
 
 
-def close_window(event=None):
-    my_message.set("{q}")
-    send_message()
-
+def on_closing(event=None):
+    
+    my_msg.set("{quit}")
+    send()
 
 top = tkinter.Tk()
 top.title("Chat Box")
 
 messages_frame = tkinter.Frame(top)
-my_message = tkinter.StringVar()
-my_message.set("Messages can be written here")
-scrollbar = tkinter.Scrollbar(messages_frame)
+my_msg = tkinter.StringVar()  
+my_msg.set("Type your messages here.")
+scrollbar = tkinter.Scrollbar(messages_frame)  
 msg_list = tkinter.Listbox(messages_frame, height=20, width=100, yscrollcommand=scrollbar.set)
 scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
 msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
 msg_list.pack()
 messages_frame.pack()
 
-message_box = tkinter.Entry(top, textvariable=my_message)
-message_box.bind("<Return>", send_message)
-message_box.pack()
-button_send = tkinter.Button(top, text="Send Message", command=send_message)
-button_send.pack()
+entry_field = tkinter.Entry(top, textvariable=my_msg)
+entry_field.bind("<Return>", send)
+entry_field.pack()
+send_button = tkinter.Button(top, text="Send", command=send)
+send_button.pack()
 
-top.protocol("WM_DELETE_WINDOW", close_window)
+top.protocol("WM_DELETE_WINDOW", on_closing)
+
 
 HOST = input('Enter server host: ')
-PORT = input('Enter server port: ')
+PORT = input('Enter serve port: ')
 if not PORT:
     PORT = 1234
 else:
     PORT = int(PORT)
 
-ADDRESS = (HOST, PORT)
 
-socket = socket(AF_INET, SOCK_STREAM)
-socket.connect(ADDRESS)
+ADDR = (HOST, PORT)
 
-thread = Thread(target=receive_message)
-thread.start()
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect(ADDR)
+
+receive_thread = Thread(target=receive)
+receive_thread.start()
 receivefile_thread = Thread(target=receive_file)
 receivefile_thread.start()
-tkinter.mainloop() 
-
-
-
+tkinter.mainloop()  
